368fb9e59aa0b233ae33b16baa18aec180f15969
1hani7 <1hani7@naver.com>
2023-07-13 08:32:01 +0900

    20230712

---
+++ 20230712/static/js/zoo.js
@@ -0,0 +1,7 @@
+function bar_open(){
+    document.getElementsByClassName("search_bar")[0].style.display="block";
+}
+
+function bar_close(){
+    document.getElementsByClassName("search_bar")[0].style.display="none";
+}
