2a9d874a5a8c26fafa54d823cd7fdc4cbc357f72
1hani7 <1hani7@naver.com>
2023-08-09 09:10:10 +0900

    20202020

--- 08/20230808/static/js/search.js
+++ 08/20230808/static/js/search.js
@@ -1,14 +1,12 @@
 let data_list = new Object;
 
 async function getData(){
-    var data = await fetch("https://api.odcloud.kr/api/15031992/v1/uddi:3c46e4ef-78dc-4da6-97b1-ad226624eff5_201911131644?page=1&perPage=500&serviceKey=ymqZ2e7%2FnpnUWYCS9B9Zv65We7NUOtTOzGLwPyNlFnegW4cGrvvzxIOnHaleT%2F3JTcgci5x%2FcdQcpdCkBVBgwA%3D%3D").then(function(res){ return res.json();}).then(function(r){return r;});
-    // console.log(data.records);
+    var data = await fetch("https://api.odcloud.kr/api/15031992/v1/uddi:3c46e4ef-78dc-4da6-97b1-ad226624eff5_201911131644?page=1&perPage=500&serviceKey=ymqZ2e7%2FnpnUWYCS9B9Zv65We7NUOtTOzGLwPyNlFnegW4cGrvvzxIOnHaleT%2F3JTcgci5x%2FcdQcpdCkBVBgwA%3D%3D").then(function(res){ return res.json();});
     return data.data;
 }
 
 
 $(async function(){
-    // console.log(getData())
 
     data_list = await getData();
     draw(data_list);
@@ -41,14 +39,13 @@ function search(){
         }
 
         if(amount.length!=0 && isShow){
-            isShow = false;
             if(Number(data_list[idx].어초확인수량) >= Number(amount[0])) isShow=true;
             else isShow = false;
         }
 
         if(year.length!=0 && isShow){
-            isShow = false;
-            if(Number(data_list[idx].설치년도) >= Number(year[0]) && Number(data_list[idx].설치년도) < Number(year[0])+100) isShow=true;
+            if(Number(data_list[idx].설치년도) >= Number(year[0]) &&
+                 Number(data_list[idx].설치년도) < Number(year[0])+100) isShow=true;
             else isShow = false;
         }
 
