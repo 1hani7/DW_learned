ec777fe648e7ff04f914c31016d72de87773a1c9
1hani7 <1hani7@naver.com>
2023-08-16 17:47:25 +0900

    20230816_afternoon

--- 08/20230816/static/js/bar.js
+++ 08/20230816/static/js/bar.js
@@ -1,9 +1,7 @@
-const tall = [ 161,168,174,159,183 ];
-const name = [ "송은선","임민지","이상준","김선향","신상수" ]
-// const avg = (arr) => arr.reduce((a,b)=>a+b)/tall.length;
+const tall = [ 161,168,174,159 ];
+const name = [ "송은선","임민지","이상준","김선향" ]
+const weight = [ 43,59,72,46 ];
 let avg = tall_avg();
-
-let coloring = [];
 
 
 $("#reg_bt").click(function(){
@@ -12,21 +10,21 @@ $("#reg_bt").click(function(){
         $("#tall").val()==''?$("#tall").focus():$("#name").focus();
         return;
     }
+    if($("#weight").val()==''){
+        alert("몸무게를 입력하세요.");
+    }
     tall.push( Number($("#tall").val()) );
     name.push( $("#name").val() );
+    weight.push(Number($("#weight").val()));
 
     chart.destroy();
-    coloring = [];
     avg = tall_avg();
-    colorPicker()
     draw();
-    console.log(coloring);
 });
 
 let ctx = $("#bar_chart")[0].getContext('2d');
 let chart = '';
-avg = tall_avg();
-colorPicker();
+avg_color();
 draw();
 
 function draw(){
@@ -35,26 +33,26 @@ function draw(){
         data:{
             labels:name,
             datasets:[{
-                label:"201호 신장 조사",
+                label:["201호 신장 조사"],
                 data:tall,
-                // borderWidth:5,
-                backgroundColor:coloring,
-                barPercentage:0.7, // 막대 너비 (퍼센티지)
-                // barThickness:100, // 막대 너비 (고정px)
-                borderColor:"skyblue",
-                borderSkipped:"start", // start, end, middle, bottom, left, top, right
-                borderRadius:5,
-                // categoryPercentage:0.1,
-                indexAxis:"x",
-                hoverBackgroundColor:"yellowgreen", // BorderColor, BorderWidth, BorderRadius 등등 가능
-                base:avg
-            }]
-        },
-        options:{
-            scales:{
-                y:{min:150, max:200, ticks:{stepSize:0.5}}
+                borderWidth:0,
+                backgroundColor:avg_color(),
+                indexAxis:"y"
+            },
+            {
+                label:"201호 무게 조사",
+                data:weight,
+                borderWidth:0,
+                backgroundColor:"skyblue",
+                indexAxis:"y"
             }
-        }
+        ]
+        },
+        // options:{
+        //     scales:{
+        //         y:{min:150, max:200, ticks:{stepSize:0.5}}
+        //     }
+        // }
     });
 }
 
@@ -64,8 +62,18 @@ function tall_avg(){
     return sum/tall.length;
 }
 
-function colorPicker(){
-    for(var i=0; i<tall.length; i++){
-        tall[i]>=avg?coloring.push("#4374D9"):coloring.push("#CC3D3D");
-    }
+function avg_color(){
+    let a = [];
+    $.each(tall,(i, item)=> item >= avg?a.push("#4374D9"):a.push("#CC3D3D"));
+    return a;
 }
+
+// barPercentage:0.5, // 막대 너비 (퍼센티지)
+// barThickness:100, // 막대 너비 (고정px)
+// borderColor:"skyblue",
+// borderSkipped:"start", // start, end, middle, bottom, left, top, right
+// borderRadius:100,
+// categoryPercentage:0.1,
+// indexAxis:"x",
+// hoverBackgroundColor:"yellowgreen", // BorderColor, BorderWidth, BorderRadius 등등 가능
+// base:avg
