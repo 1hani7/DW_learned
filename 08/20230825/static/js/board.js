4cdd7a25e2b2bd6ba45e2159933b4899f9c9658f
1hani7 <1hani7@naver.com>
2023-08-25 15:51:53 +0900

    20230825_afternoon

---
+++ 08/20230825/static/js/board.js
@@ -0,0 +1,19 @@
+const board_img=["카리나1.jpg","카리나2.jpg","카리나3.jpg","강동원.jpg","김채원.jpg","데인드한.jpg","미연.jpg","미츠하.jpg",
+"백지헌.jpg","산.jpg","설리.jpg","소연.jpg","소피.jpg","시타.jpg","아리에티.jpg","아이유.jpg","엠버.jpg","유체리.jpg",
+"잘몰름.jpg","정우성.jpg","채영.jpg","키키.jpg","티모시.jpg","프로도.jpg","한소희.jpg","혜린1.jpg","혜린2.jpg","히나.jpg"];
+
+const board=new Array(28);
+var gamer=[];
+
+$(function(){
+    board.fill(0);
+    initBoard();
+    draw();
+    $("#setBt").click(setOpen);
+    t=setInterval( ()=>{
+        if( gamer.length > 0 ){
+            console.log(gamer);
+            clearInterval(t);
+        }
+    }, 50 )
+});
