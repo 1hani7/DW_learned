44b2f1f997a098dd547a6e8131f8cf22c4f5d830
1hani7 <1hani7@naver.com>
2023-08-28 09:49:01 +0900

    641615

---
+++ 08/20230828/static/js/main.js
@@ -0,0 +1,6 @@
+let num = 100;
+
+$(function(){
+    output();
+    output2();
+});
---
+++ 08/20230828/static/js/mdtest.js
@@ -0,0 +1,7 @@
+function output(){
+    console.log("출력이 되네요");
+}
+
+function output2(){
+    console.log("전역변수 num : "+num);
+}
