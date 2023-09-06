2c4ad9db69ceef30a392c85f3ecb3ec4cf8d4f48
1hani7 <1hani7@naver.com>
2023-09-06 15:15:27 +0900

    vue

--- NodeJS/TodoList/src/components/TodoListNew.vue
+++ NodeJS/TodoList/src/components/TodoListNew.vue
@@ -1,8 +1,49 @@
 <template>
-    
+    <section class="mb-5">
+        <div class="container">
+            <div class="row justify-content-center m-2">
+                <div class="col border border-primary rounded">
+                    <input type="text" id="todo_input" class="form-control my-2"
+                    v-model="job" placeholder="할 일을 작성하시게"/>
+                </div>
+                <div class="row my-2">
+                    <div class="col-6">
+                        <input type="date" v-model="date" :min="today">
+                    </div>
+                    <div class="col-6">
+                        <button type="button" class="btn btn-primary btn-sm float-end"
+                        @click="onAddTodo">할일추가</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
+
+import{ reactive, toRefs, inject } from 'vue' // inject 쓰려면 vue에서 import 해줘야 함
+
+export default{
+    name:"TodoListNew",
+    setup(){
+        const today = inject('today')
+        const addTodo = inject('addTodo');
+        const val_obj = reactive({
+            job:'', date:today, today:today,
+        })
+        const onAddTodo=()=>{
+            if( val_obj.job.length > 0 ){
+                addTodo(val_obj.job, val_obj.date)
+                val_obj.date=''
+                val_obj.date=today
+            }
+        }
+        return{ // 다른 파일이 아니라 이 파일의 template에 있는 html로 보내주는 return
+            ...toRefs(val_obj),onAddTodo
+        }
+    }
+}
 
 </script>
 
--- NodeJS/TodoList/src/views/TodoListContainer.vue
+++ NodeJS/TodoList/src/views/TodoListContainer.vue
@@ -16,6 +16,40 @@
         name : "TodoListContainer",
         components: {
             TodoListNew, TodoListMain
+        },
+        setup(){
+            const todos = ref([])
+            const { loadTodos, saveTodos, storage_id } = useStorage()
+            provide('todos',readonly(todos))
+
+            const initTodos = (init_todos) => {
+                todos.value = init_todos
+            }
+            const addTodo = (job, date) => { // 할일 추가 기능
+                todos.value.push({
+                    id: storage_id.value++,
+                    job: job,
+                    date: date,
+                    completed: false,
+                })
+                saveTodos(todos)
+            }
+            const removeTodo = (id) => { // 할일 삭제 기능
+                todos.value.splice(id,1)
+                todos.value.forEach( (todo, box) => {
+                    todo.id=idx
+                })
+                saveTodos(todos)
+            }
+            const completeTodo = (id) =>{
+                todos.value.find( (todo) => todo.id == id ).completed=true
+                saveTodos(todos)
+            }
+            provide('addTodo', addTodo)
+            provide('removeTodo', removeTodo)
+            provide('completeTodo', completeTodo)
+
+            loadTodos(initTodos)
         }
     }
 </script>
