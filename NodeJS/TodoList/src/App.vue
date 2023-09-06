3992dda8edf68f0c4625348f41003f54ddbd058b
1hani7 <1hani7@naver.com>
2023-09-06 13:56:37 +0900

    20230906_afternoon

---
+++ NodeJS/TodoList/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+  import { inject } from 'vue';
+import { RouterView } from 'vue-router';
+  
+  export default{
+    setup() {
+        const today = inject('today');
+        return { today };
+    },
+    components: { RouterView }
+}
+</script>
+
+<template>
+  <header>
+    <hgroup class="my-5">
+      <h1>나의 할 일</h1>
+      <em>{{ today }}</em>
+    </hgroup>
+    <RouterView/>
+  </header>
+</template>
+
+<style scoped>
+hgroup{
+  text-align:center;
+  font-family:'Arial Bold';
+}
+hgroup h1{
+  font-weight:bolder;
+}
+</style>
